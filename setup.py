bac6edc0ef4f6096b2f8671394fc2481b83cd24f
Sylvain Viollon <thefunny@gmail.com>
2009-10-21 15:38:56 +0000

    Silva Translation

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.2b1dev'
+
+setup(name='silva.translations',
+      version=version,
+      description="Translation for Silva CMS",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='silva translations',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['silva'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
